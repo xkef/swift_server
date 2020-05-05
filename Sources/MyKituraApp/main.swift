ce375efc35c3d8ef8b942f9662a1f2a7ce2e7efc
Kevin Küng <kevinkkueng@gmail.com>
2020-05-05 03:25:02 +0200

    init server

---
+++ Sources/MyKituraApp/main.swift
@@ -0,0 +1,13 @@
+import Kitura
+import LoggerAPI
+import Application
+
+do {
+    
+    let app = try App()
+    
+    try app.run()
+
+} catch let error {
+    Log.error(error.localizedDescription)
+}
